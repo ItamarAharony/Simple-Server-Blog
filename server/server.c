6c1abd505bbb382a9a39a4bbd069cdc7158b1c4a
ItamarAharony <itamar.aharony.ia@gmail.com>
2024-08-26 18:30:49 -0400

    modified server.c to broadcast messages to other clients

--- server/server.c
+++ server/server.c
@@ -2,15 +2,29 @@
 #include "queue.h"
 #include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 #define PORT 12345
+#define MAX_CLIENTS 10
+
+int client_sockets[MAX_CLIENTS];
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *handle_client(void *client_socket);
+void broadcast_message(const char *message, int sender_socket);
 
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
+
+    // Initialize client sockets array
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        client_sockets[i] = 0;
+    }
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
@@ -31,11 +45,24 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    printf("Server is listening on port %d\n", PORT);
+
     while (1) {
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
+
+        pthread_mutex_lock(&clients_mutex);
+        // Add new socket to the client sockets array
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (client_sockets[i] == 0) {
+                client_sockets[i] = new_socket;
+                printf("New client connected: socket %d\n", new_socket);
+                break;
+            }
+        }
+        pthread_mutex_unlock(&clients_mutex);
 
         pthread_t tid;
         if (pthread_create(&tid, NULL, handle_client, (void *)&new_socket) != 0) {
@@ -54,11 +81,32 @@ void *handle_client(void *client_socket) {
     while ((valread = read(sock, buffer, 1024)) > 0) {
         buffer[valread] = '\0';
         printf("Received: %s\n", buffer);
-        // Send the message to all connected clients (this part is simplified)
-        send(sock, buffer, strlen(buffer), 0);
+
+        // Broadcast the message to all clients except the sender
+        broadcast_message(buffer, sock);
     }
+
+    // Remove the socket from the client sockets array
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (client_sockets[i] == sock) {
+            client_sockets[i] = 0;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
 
     close(sock);
     return NULL;
+}
+
+void broadcast_message(const char *message, int sender_socket) {
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (client_sockets[i] != 0 && client_sockets[i] != sender_socket) {
+            send(client_sockets[i], message, strlen(message), 0);
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
 }
 
